9934d343485b0f65bea910d657836dad9584e9b5
Cornelius Smith <smithcornelius78@gmail.com>
2020-08-28 12:50:14 -0400

    Added basic testing for App.tsx

--- native/src/App.test.tsx
+++ native/src/App.test.tsx
@@ -1,11 +1,35 @@
 import React from 'react';
-import renderer from 'react-test-renderer';
+import renderer, { act } from 'react-test-renderer';
+import { Platform } from 'react-native';
+import { App, getHeaderColorForPlatform } from './App';
 
-import { App } from './App';
-
-describe('<App />', () => {
-  test('renders without crashing', () => {
-    const tree = renderer.create(<App />).toJSON();
-    expect(tree).not.toBeNull();
+describe('<App/>', () => {
+  const tree = renderer.create(<App />).toJSON();
+  test('if it renders without crashing', async () => {
+    await act(async ()=>{
+      expect(tree).not.toBeNull();
+    });
   });
+
+  test('if it matches the snapshot', async () => {
+    await act(async ()=>{
+      expect(tree).toMatchSnapshot();
+    });
+  });
+
+});
+
+describe('getHeaderColorForPlatform', () => {
+  test('if it returns the right colors', () => {
+
+    Platform.OS = 'android'
+    const resultAndroid: string = getHeaderColorForPlatform();
+      
+    Platform.OS = 'ios';
+    const resultiOS: string = getHeaderColorForPlatform();
+
+    expect(resultAndroid).toBe("#ff8159")
+    expect(resultiOS).toBe("#f4511e")
+  });
+
 });
